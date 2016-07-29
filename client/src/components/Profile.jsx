65b0da28d42b8dafeceeb6e0f37d731af0e30ab2
rhanzawa <rhanzawa@gmail.com>
2016-07-28 17:34:28 -0700

    Fix merge conflict

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,38 +1,65 @@
 import React from 'react';
+
 import { getUsers, getMessages, filterMessages } from '../util/profileUtil.js';
+
+import MessageStream from './MessageStream';
+
+////////////// MOCK DATA ////////////////////////
+const messages = [
+  {
+    url: 'http://materializecss.com/images/sample-1.jpg',
+    type: 'image',
+    createdAt: '2016-07-26',
+    username: 'Robb',
+  },
+  {
+    url: 'http://materializecss.com/images/sample-1.jpg',
+    type: 'image',
+    createdAt: '2016-07-27',
+    username: 'Greg',
+  },
+  {
+    url: 'http://materializecss.com/images/sample-1.jpg',
+    type: 'image',
+    createdAt: '2016-07-28',
+    username: 'Robb',
+  },
+];
+////////////// MOCK DATA ////////////////////////
+
 class Profile extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       allUsers: [],
-      currentUser: 'hard-coded-user', // this.props.currentUser?
+      currentUser: 'Greg', // this.props.currentUser?
       allMessages: [],
       currentMessages: [],
       showLoading: true,
-    }
+    };
   }
 
-  componentDidMount() {
-    // ASYNC, Y U DO THIS
-    // first load users
-    getUsers()
-      .then((users) => {
-        this.setState({
-          allUsers: users,
-        });
-      }).then(() => {
-        //once users has been loaded, load messages
-        getMessages()
-          .then((messages) => {
-            //once messages have been loaded remove loading notification
-            this.setState({
-              allMessages: messages,
-              showLoading: false,
-            });
-          });
-      });
-  }
+  // componentDidMount() {
+  //   // ASYNC, Y U DO THIS
+  //   // first load users
+  //   getUsers()
+  //     .then((users) => {
+  //       this.setState({
+  //         allUsers: users,
+  //       });
+  //     }).then(() => {
+  //       //once users has been loaded, load messages
+  //       getMessages()
+  //         .then((messages) => {
+  //           //once messages have been loaded remove loading notification
+  //           this.setState({
+  //             allMessages: messages,
+  //             showLoading: false,
+  //           });
+  //         });
+  //     });
+  // }
 
   handleClick(e) {
     const otherUser = e.target.textContet;
@@ -42,26 +69,26 @@ class Profile extends React.Component {
     });
   }
 
-  render () {
+  render() {
     return (
-      <div className="container">
-        <div className="sidebar">
-          <p className="sidebar-entry">USERS</p>
-          <p className="sidebar-entry" onClick={this.handleClick} >Ryan</p>
-          <p className="sidebar-entry" onClick={this.handleClick} >Robb</p>
-          <p className="sidebar-entry" onClick={this.handleClick} >John Cena</p>
+      <div>
+        <div className="row">
+          <h3>Messages</h3>
         </div>
-        
-        <div className="main">
-          <ul className="message-list">
-            <li className="current-user">@greg: video, url: goodmorning</li>
-            <li className="current-user">@greg: video, url: goodnight</li>
-            <li className="other-user">@ryan: video, url: goodmorning</li>
-          </ul>
+        <div className="row message-box">
+          <div className="col s3">
+            <ul className="collection">
+              <li><a className="collection-item light-blue-text">Ryan</a></li>
+              <li><a className="collection-item light-blue-text">Robb</a></li>
+              <li><a className="collection-item light-blue-text">John Cena</a></li>
+            </ul>
+          </div>
+
+          <MessageStream currentUser={this.state.currentUser} messages={messages} />
         </div>
       </div>
     );
   }
-};
+}
 
 export default Profile;
