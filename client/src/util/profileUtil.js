5f689fe0c59908191f3e11985d961bcb6f7d83e4
Greg Roche <grroche.us@gmail.com>
2016-07-29 22:04:27 -0700

    Resolve merge conflict

--- client/src/util/profileUtil.js
+++ client/src/util/profileUtil.js
@@ -4,11 +4,11 @@ function getUsers() {
       type: 'GET',
       url: "/api/users",
       success: (data) => {
-        console.log('Got user data from server')
+        console.log('Got user data from server');
         const users = [];
         for (let i = 0; i < data.length; i++) {
           users.push(data[i].username);
-        } 
+        }
         resolve(data);
       },
       error: (err) => {
@@ -17,49 +17,70 @@ function getUsers() {
       },
     });
   });
-};
+}
 
 function getMessages() {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     $.ajax({
       type: 'GET',
       url: "/api/messages",
       success: (data) => {
-        console.log('Got messages data from server')
+        console.log('Got messages data from server');
         resolve(data);
       },
       error: (err) => {
         console.log('Unable to retrieve messages: ', err);
         reject('Unable to retrieve messages:', err);
-      }
+      },
     });
   });
-};
+}
 
 function filterMessages(messages, currentUser, otherUser) {
-  let container = [];
+  const container = [];
   messages.forEach((message) => {
     if (message.Receiver.username === otherUser && message.Sender.username === currentUser) {
       container.push({
         url: message.url,
         type: message.type,
         createdAt: message.createdAt,
-        username: currentUser
+        username: currentUser,
       });
-    } else if (message.Receiver.username === currentUser && message.Sender.username === otherUser){
+    } else if (message.Receiver.username === currentUser && message.Sender.username === otherUser) {
       container.push({
         url: message.url,
         type: message.type,
         createdAt: message.createdAt,
-        username: otherUser
-      })
+        username: otherUser,
+      });
     }
   });
   return container;
-};
+}
+
+function createMessage(info) {
+  console.log('TEST POST', info);
+  // const data = {
+  //   info,
+  // };
+
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      type: 'POST',
+      url: '/api/messages',
+      data: info,
+      success: (res) => {
+        console.log('Sending new message to server');
+        resolve(res);
+      },
+      error: reject,
+    });
+  });
+}
 
 export {
   getUsers,
   getMessages,
   filterMessages,
+  createMessage,
 };
--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,29 +1,87 @@
 const db = require('../db/db.js');
+const session = require('express-session');
+
+function getUsers(req, res) {
+  // console.log('Session: ', req.session);
+  // console.log('User: ', req.session.user);
+  db.User.findAll({
+    where: {
+      username: {
+        $ne: req.session.user,
+      },
+    },
+  })
+  .then((users) => {
+    res.send(users);
+  });
+}
 
 function getMessages(req, res) {
-	console.log('Getting messages...');
-	db.Message.findAll({
-		include: [
-			{model: db.User, as: "Sender", attributes: ['username']},
-			{model: db.User, as: "Receiver", attributes: ['username']}
-		]
-	}).then(function(messages){
-			res.send(messages);
-	});
-};
+  console.log('Getting messages...');
+  db.Message.findAll({
+    include: [
+      { model: db.User, as: "Sender", attributes: ['username'] },
+      { model: db.User, as: "Receiver", attributes: ['username'] },
+    ],
+  }).then((messages) => {
+    res.send(messages);
+  });
+}
 
 function createMessage(req, res) {
-	// ?
-	const { type, url, SenderId, ReceiverId } = req.body.info;
-	db.Message.create({
-		type, 
-		url,
-		SenderId,
-		ReceiverId,
-	}).then(() => {
-		res.send({
-			success: 'message created',
-			url: url
-		})
-	});
+  // ?
+  const type = req.body.type;
+  const url = req.body.url;
+
+
+  function getSender() {
+    return db.User.findOne({
+      attributes: ['id'],
+      where: {
+        username: [req.body.senderName],
+      },
+    });
+  }
+  function getReceiver() {
+    return db.User.findOne({
+      attributes: ['id'],
+      where: {
+        username: [req.body.receiverName],
+      },
+    });
+  }
+
+  Promise.all([getSender(), getReceiver()])
+    .then((values) => {
+      const SenderId = values[0].id;
+      const ReceiverId = values[1].id;
+      return db.Message.create({
+        type,
+        url,
+        SenderId,
+        ReceiverId,
+      });
+    })
+    .catch(console.error.bind(console))
+    .then(() => {
+    // res.send({
+    //   success: 'message created',
+    // });
+      res.send('success');
+    });
 }
+
+// db.User.findOne({
+//   attribute: ['id', 'username'],
+//   where: {
+//     username: ["Robb"],
+//   },
+// }).then((data) => {
+//   console.log('Name :', data.username, ' ,ID: ', data.id);
+// });
+
+module.exports = {
+  getUsers,
+  getMessages,
+  createMessage,
+};
