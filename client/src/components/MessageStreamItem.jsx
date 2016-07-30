bd1d45cc37e6814a25e86ec30b0c2a24874cee21
Greg Roche <grroche.us@gmail.com>
2016-07-29 21:52:51 -0700

    Begin to build video and gif buttons

--- client/src/components/MessageStreamItem.jsx
+++ client/src/components/MessageStreamItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import VideoButton from 'VideoButton';
+
 function MessageStreamItem(props) {
   const className = props.message.username === props.currentUser ? 'user-me' : 'user-other';
   // "http://materializecss.com/images/sample-1.jpg"
@@ -12,6 +14,17 @@ function MessageStreamItem(props) {
         <div className="card-image">
           <p>url: {props.message.url}, type: {props.message.type}</p>
         </div>
+      </div>
+      <div className="fixed-action-btn horizontal" style={ {right: "150px"} }  >
+        <a className="btn-floating btn-large red">
+          <i className="large material-icons">videocam</i>
+        </a>
+        <ul>
+          <li><a className="btn-floating red">POST</a></li>
+          <li><a className="btn-floating red"><i className="material-icons">replay</i></a></li>
+          <li><a className="btn-floating red"><i className="material-icons">stop</i></a></li>
+          <li><a className="btn-floating red"><i className="material-icons">album</i></a></li>
+        </ul>
       </div>
     </li>
   );
