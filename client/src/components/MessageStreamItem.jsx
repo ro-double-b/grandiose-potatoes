bd55c453488672a8666a4a792b1d9525d75689ea
Greg Roche <grroche.us@gmail.com>
2016-07-29 22:07:06 -0700

    Resolve merge conflicts

--- client/src/components/MessageStreamItem.jsx
+++ client/src/components/MessageStreamItem.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 function MessageStreamItem(props) {
   const className = props.message.username === props.currentUser ? 'user-me' : 'user-other';
+  // "http://materializecss.com/images/sample-1.jpg"
+  // <img src="http://materializecss.com/images/sample-1.jpg" alt="Video message" />
+  console.log(props.message);
 
   return (
     <li className={className}>
-      <img src="http://materializecss.com/images/sample-1.jpg" alt="Video message" />
+      <div className="card">
+        <div className="card-image">
+          <p>url: {props.message.url}, type: {props.message.type}</p>
+        </div>
+      </div>
     </li>
   );
 }
--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 function User(props) {
-  // onClick={props.handleClick}
   return (
-    <li><a className="collection-item light-blue-text">{props.otherUserName}</a></li>
+    <li><a onClick={props.handleClick} className="collection-item light-blue-text">{props.otherUserName}</a></li>
   );
 }
 
 User.propTypes = {
   otherUserName: React.PropTypes.string,
-  // handleClick: React.PropTypes.func,
+  handleClick: React.PropTypes.func,
 };
 
 export default User;
