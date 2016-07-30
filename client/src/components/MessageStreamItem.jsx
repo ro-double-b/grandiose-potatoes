d7d6079e67fa7ccc432199cb7566403d3eadffdc
Robb Marsee <robb.marsee@gamil.com>
2016-07-30 12:33:06 -0700

    update style for gifs

--- client/src/components/MessageStreamItem.jsx
+++ client/src/components/MessageStreamItem.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
-import VideoButton from 'VideoButton';
+// import VideoButton from 'VideoButton';
 
 function MessageStreamItem(props) {
   const className = props.message.username === props.currentUser ? 'user-me' : 'user-other';
   // "http://materializecss.com/images/sample-1.jpg"
   // <img src="http://materializecss.com/images/sample-1.jpg" alt="Video message" />
   // console.log(props.message);
+
+
   return (
     <li className={className}>
       <div className="card">
         <div className="card-image">
-          <video src={props.message.url} height="360" width="480" controls></video>
+        {
+          props.message.type === "video" ? <video src={props.message.url} height="360" width="480" controls></video> : <img src={props.message.url} alt="gif-display" />
+        }
         </div>
       </div>
     </li>
